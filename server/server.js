cee4adcf60d59c14d7e0246db1ad26ad27f8b347
Mohit Pal <mohitpal2621@gmail.com>
2024-03-22 02:52:24 +0530

    test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000; // Define the port, or use 3000 by default
+const port = 3000; // Define the port, or use 3000 by default
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
