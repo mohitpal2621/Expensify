d942b6cfb18bef08711a6a11728e0d8aa5f23e38
Mohit Pal <mohitpal2621@gmail.com>
2024-03-22 09:20:25 +0530

    Format date and amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,27 @@
 import React from "react";
+import moment from "moment";
 import { Link } from "react-router-dom";
+
+const curr = Intl.NumberFormat("en-IN", {
+    style: "currency",           //Decimal, percent or currency
+    currency: "INR",             //ISO code for countries' currencies
+    currencyDisplay: "symbol",   //symbol, code, name
+    useGrouping: "auto",
+    minimumIntegerDigits: 1,
+    maximumFractionDigits: 10,
+    minimumFractionDigits: 2,
+});
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {curr.format(amount)} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
 
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -4,6 +4,7 @@ import ExpenseListItem from "../../components/ExpenseListItem";
 import expenses from '../fixtures/expenses';
 import { MemoryRouter } from 'react-router-dom';
 
+
 test("Should render a list item with given expense", () => {
     const {container} = render(<MemoryRouter><ExpenseListItem {...expenses[0]} /></MemoryRouter>);
     expect(container).toMatchSnapshot();
