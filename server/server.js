502b060d5001a9c26022774a75db5596d5f94d4a
Mohit Pal <mohitpal2621@gmail.com>
2024-04-11 07:04:56 +0530

    Modal

--- server/server.js
+++ server/server.js
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000; // Define the port, or use 3000 by default
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve the index.html file for all requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,16 +1,27 @@
 // Using Hooks instead of connect()
-import React from "react";
+import React, {useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import ExpenseForm from "./ExpenseForm";
 import { startEditExpense, startRemoveExpense } from "../actions/expenses";
+import Modal from 'react-modal';
 
 export const EditExpense = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
     const nav = useNavigate();
     const exp = useSelector(state => state.expenses.find((exp) => exp.id === id));
-    
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+
     return (
         <div>
             <div className="page-header">
@@ -26,13 +37,27 @@ export const EditExpense = () => {
                         nav("/");
                     }}
                 />
-                <button className="button button--secondary" onClick={() => {
-                        dispatch(startRemoveExpense(id));
-                        nav("/");
-                    }}
+                <button className="button button--secondary" onClick={openModal}
                 >
                     Remove
                 </button>
+                <Modal
+                    className="modal"
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Confirm Remove Expense"
+                >
+                    <h2 className="modal-header">Confirm Remove Expense</h2>
+                    <p className="modal-line">Are you sure you want to remove this expense?</p>
+                    <button className="modal-yes" onClick={() => {
+                        dispatch(startRemoveExpense(id));
+                        nav("/");
+                        closeModal();
+                    }}>
+                        Yes
+                    </button>
+                    <button className="modal-cancel" onClick={closeModal}>Cancel</button>
+                </Modal>
             </div>
         </div>
     );
